bad4855ff4b10db772efd028887de93b70f1b582
ndudefaces <121698953+ndudefaces@users.noreply.github.com>
2023-06-13 18:59:02 -0500

    Add querySelectors for each button

---
+++ main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function(){
+    //store all components from HTML into JS
+    let clear = document.querySelector('#clear-btn');
+    let equal = document.querySelector('.equal');
+    let decimal = docment.querySelector('.decimal');
+
+    let numbers = document.querySelectorAll('.number');
+    let operators = document.querySelectorAll('.operator');
+})
